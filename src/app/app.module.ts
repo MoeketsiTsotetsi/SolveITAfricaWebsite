cff9615dfca418cdfe01562565ca36f8dc7acee0
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2022-03-30 17:44:24 +0200

    designed the navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainComponent,
+    HomeComponent,
+    AppComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
